bb386ea5c096f68f19a264617f849de4c1982c7b
wolfden <kjs@photosbykjs.us>
2018-09-03 10:20:36 -0500

    graphic file for efi boot

--- forensics-spin/pre_iso_script.sh
+++ forensics-spin/pre_iso_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp -v /home/gru/Git/Coding/forensics-spin/files/isolinux/back.jpg "${CDROOT_DIR}/isolinux/back.jpg"
-
+cp -v /home/gru/Git/Coding/forensics-spin/files/boot/grub/default-splash.png "${CDROOT_DIR}/boot/grub/default-splash.png"
