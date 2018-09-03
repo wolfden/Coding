4a2ec92b11cecf3aca438e48541a9ed5f49b0976
wolfden <kjs@photosbykjs.us>
2018-09-03 10:30:19 -0500

    remove gsettings - doesn't work

--- forensics-spin/remaster_post.sh
+++ forensics-spin/remaster_post.sh
@@ -7,11 +7,6 @@ rm -rf "${CHROOT_PKGS_DIR}"{,-nonfree,-restricted}/*
 cp -rv /home/gru/Git/Coding/forensics-spin/files/usr "${CHROOT_DIR}/"
 cp -rv /home/gru/Git/Coding/forensics-spin/files/etc "${CHROOT_DIR}/"
 sudo glib-compile-schemas "${CHROOT_DIR}/usr/share/glib-2.0/schemas/"
-gsettings set org.gnome.shell.extensions.dash-to-dock extended-height true
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT
-gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DYNAMIC
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
 
 is_64=$(file "${CHROOT_DIR}"/bin/bash | grep "x86-64")
 if [ -n "${is_64}" ]; then
